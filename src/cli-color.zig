bc432f7901671527767e38f09c7c2313122c17c0
apple-x-co <apple.x.co@gmail.com>
2022-08-16 09:10:49 +0900

    refactor

--- src/cli-color.zig
+++ src/cli-color.zig
@@ -1,69 +1,79 @@
+const std = @import("std");
+
 pub fn reset() []const u8 {
     return "\u{001b}[0m";
 }
 
-pub fn fgBlack(text: anytype) []const u8 {
-    return "\u{001b}[30m" ++ text;
+fn format(comptime fmt: []const u8, text: []const u8) []const u8 {
+    var allocator = std.heap.page_allocator;
+    const buffer = std.fmt.allocPrint(allocator, fmt, .{text}) catch @panic("allocPrint");
+    // defer allocator.free(buffer);
+
+    return buffer;
+}
+
+pub fn fgBlack(text: []const u8) []const u8 {
+    return format("\u{001b}[30m{s}", text);
 }
 
 pub fn fgRed(text: anytype) []const u8 {
-    return "\u{001b}[31m" ++ text;
+    return format("\u{001b}[31m{s}", text);
 }
 
 pub fn fgGreen(text: anytype) []const u8 {
-    return "\u{001b}[32m" ++ text;
+    return format("\u{001b}[32m{s}", text);
 }
 
 pub fn fgYellow(text: anytype) []const u8 {
-    return "\u{001b}[33m" ++ text;
+    return format("\u{001b}[33m{s}", text);
 }
 
 pub fn fgBlue(text: anytype) []const u8 {
-    return "\u{001b}[34m" ++ text;
+    return format("\u{001b}[34m{s}", text);
 }
 
 pub fn fgMagenta(text: anytype) []const u8 {
-    return "\u{001b}[35m" ++ text;
+    return format("\u{001b}[35m{s}", text);
 }
 
 pub fn fgCyan(text: anytype) []const u8 {
-    return "\u{001b}[36m" ++ text;
+    return format("\u{001b}[36m{s}", text);
 }
 
 pub fn fgWhite(text: anytype) []const u8 {
-    return "\u{001b}[37m" ++ text;
+    return format("\u{001b}[37m{s}", text);
 }
 
 pub fn bgBlack(text: anytype) []const u8 {
-    return "\u{001b}[40m" ++ text;
+    return format("\u{001b}[40m{s}", text);
 }
 
 pub fn bgRed(text: anytype) []const u8 {
-    return "\u{001b}[41m" ++ text;
+    return format("\u{001b}[41m{s}", text);
 }
 
 pub fn bgGreen(text: anytype) []const u8 {
-    return "\u{001b}[42m" ++ text;
+    return format("\u{001b}[42m{s}", text);
 }
 
 pub fn bgYellow(text: anytype) []const u8 {
-    return "\u{001b}[43m" ++ text;
+    return format("\u{001b}[43m{s}", text);
 }
 
 pub fn bgBlue(text: anytype) []const u8 {
-    return "\u{001b}[44m" ++ text;
+    return format("\u{001b}[44m{s}", text);
 }
 
 pub fn bgMagenta(text: anytype) []const u8 {
-    return "\u{001b}[45m" ++ text;
+    return format("\u{001b}[45m{s}", text);
 }
 
 pub fn bgCyan(text: anytype) []const u8 {
-    return "\u{001b}[46m" ++ text;
+    return format("\u{001b}[46m{s}", text);
 }
 
 pub fn bgWhite(text: anytype) []const u8 {
-    return "\u{001b}[47m" ++ text;
+    return format("\u{001b}[47m{s}", text);
 }
 
 // pub fn hello() std.mem.Allocator.Error![]u8 {
--- src/main.zig
+++ src/main.zig
@@ -1,36 +1,31 @@
 const std = @import("std");
 const color = @import("cli-color.zig");
 
-// FIXME: "color.reset()" などを連結しないで出力すると、以下エラーが出る
-// ./src/cli-color.zig:6:30: error: unable to evaluate constant expression
-// return "\u{001b}[30m" ++ text;
-
 // FIXME: 引数の text を anytype から []const u8 にすると、以下エラーが出る
 // ./src/cli-color.zig:6:30: error: unable to evaluate constant expression
 // return "\u{001b}[30m" ++ text;
 
 pub fn main() anyerror!void {
     // Foreground color
-    const stdout = std.io.getStdOut();
-    try stdout.writeAll(color.fgBlack("Foreground BLACK") ++ color.reset() ++ "\n");
-    try stdout.writeAll(color.fgRed("Foreground RED") ++ color.reset() ++ "\n");
-    try stdout.writeAll(color.fgGreen("Foreground GREEN") ++ color.reset() ++ "\n");
-    try stdout.writeAll(color.fgYellow("Foreground YELLO") ++ color.reset() ++ "\n");
-    try stdout.writeAll(color.fgBlue("Foreground BLUE") ++ color.reset() ++ "\n");
-    try stdout.writeAll(color.fgMagenta("Foreground MAGENTA") ++ color.reset() ++ "\n");
-    try stdout.writeAll(color.fgCyan("Foreground CYAN") ++ color.reset() ++ "\n");
-    try stdout.writeAll(color.fgWhite("Foreground WHITE") ++ color.reset() ++ "\n");
+    std.debug.print("{s}{s}\n", .{color.fgBlack("Foreground BLACK"), color.reset()});
+    std.debug.print("{s}{s}\n", .{color.fgRed("Foreground RED"), color.reset()});
+    std.debug.print("{s}{s}\n", .{color.fgGreen("Foreground GREEN"), color.reset()});
+    std.debug.print("{s}{s}\n", .{color.fgYellow("Foreground YELLO"), color.reset()});
+    std.debug.print("{s}{s}\n", .{color.fgBlue("Foreground BLUE"), color.reset()});
+    std.debug.print("{s}{s}\n", .{color.fgMagenta("Foreground MAGENTA"), color.reset()});
+    std.debug.print("{s}{s}\n", .{color.fgCyan("Foreground CYAN"), color.reset()});
+    std.debug.print("{s}{s}\n", .{color.fgWhite("Foreground WHITE"), color.reset()});
 
-    // Background color
-    std.debug.print("{s}\n", .{color.bgBlack("Background BLACK") ++ color.reset()});
-    std.debug.print("{s}\n", .{color.bgRed("Background RED") ++ color.reset()});
-    std.debug.print("{s}\n", .{color.bgGreen("Background GREEN") ++ color.reset()});
-    std.debug.print("{s}\n", .{color.bgYellow("Background YELLOW") ++ color.reset()});
-    std.debug.print("{s}\n", .{color.bgBlue("Background BLUE") ++ color.reset()});
-    std.debug.print("{s}\n", .{color.bgMagenta("Background MAGENTA") ++ color.reset()});
-    std.debug.print("{s}\n", .{color.bgCyan("Background CYAN") ++ color.reset()});
-    std.debug.print("{s}\n", .{color.bgWhite("Background WHITE") ++ color.reset()});
+    // // Background color
+    std.debug.print("{s}{s}\n", .{color.bgBlack("Background BLACK"), color.reset()});
+    std.debug.print("{s}{s}\n", .{color.bgRed("Background RED"), color.reset()});
+    std.debug.print("{s}{s}\n", .{color.bgGreen("Background GREEN"), color.reset()});
+    std.debug.print("{s}{s}\n", .{color.bgYellow("Background YELLOW"), color.reset()});
+    std.debug.print("{s}{s}\n", .{color.bgBlue("Background BLUE"), color.reset()});
+    std.debug.print("{s}{s}\n", .{color.bgMagenta("Background MAGENTA"), color.reset()});
+    std.debug.print("{s}{s}\n", .{color.bgCyan("Background CYAN"), color.reset()});
+    std.debug.print("{s}{s}\n", .{color.bgWhite("Background WHITE"), color.reset()});
 
     // Mix
-    try stdout.writeAll(color.bgBlack(color.fgCyan("Background BLACK x Foreground CYAN")) ++ color.reset() ++ "\n");
+    std.debug.print("{s}{s}\n", .{color.bgBlack(color.fgCyan("Background BLACK x Foreground CYAN")), color.reset()});
 }
